ffa8f19b004a5b7855e176d2aad73e1e0db7eaf5
Brian Ellin <brianellin@gmail.com>
2024-10-29 14:59:18 -0700

    add dot to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { BookHeart, Dog } from "lucide-react";
+import { BookHeart, Dog, Dot } from "lucide-react";
 import Link from "next/link";
 
 export default function Header() {
@@ -6,7 +6,8 @@ export default function Header() {
     <header className="bg-white text-black p-4 shadow-sm">
       <div className="container mx-auto flex justify-center">
         <Link href="/" className="inline-flex items-center">
-          <BookHeart className="w-5 h-5 mr-2" />
+          <BookHeart className="w-5 h-5" />
+          <Dot className="w-5 h-5" />
           <Dog className="w-5 h-5" />
         </Link>
       </div>
