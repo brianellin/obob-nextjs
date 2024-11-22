3b073694a47002e0873208bd125b9a3a96898214
Brian Ellin <brianellin@gmail.com>
2024-11-21 17:08:20 -0800

    Improve loading and caching of cover images

--- components/BookSelection.tsx
+++ components/BookSelection.tsx
@@ -151,6 +151,8 @@ export default function BookSelection({
                     layout="fill"
                     objectFit="cover"
                     className="rounded-md shadow-sm"
+                    priority={true}
+                    loading="eager"
                   />
                   {(!isMobile || !selectedBookKeys.includes(key)) && (
                     <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-200">
