8163832d54e43c7043447635a0665819face997c
Brian Ellin <brianellin@gmail.com>
2025-09-08 14:13:50 -0700

    Fix build

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { Button } from "@/components/ui/button";
